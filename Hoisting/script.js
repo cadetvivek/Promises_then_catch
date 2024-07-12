f1f98a2d73f46b46c4a33137b3e38319b8292d02
cadetvivek <kushwahvivek805@gmail.com>
2024-07-12 15:51:52 +0530

    Hoisting

---
+++ Hoisting/script.js
@@ -0,0 +1,34 @@
+// console.log(a);
+// let a = 10  // can not be accesss before initilization
+
+// console.log(b) // hoisting method apply method
+// var b = 20 
+
+
+// var x = 10;
+// function userName(){
+//     console.log('my name is vivek')
+// }
+// userName()
+// console.log(x)
+
+
+// userName()// my ane is vivek
+// console.log(x)//undefined
+// var x = 10;
+// function userName(){
+//     console.log('my name is vivek')
+// }
+
+
+
+
+userName()
+console.log(x)
+console.log(userName)    
+var x = 10;
+const userName = ()=>{
+    console.log('my name is vivek')
+
+}//we get ref error becasue it's react just like a 
+// variable userName here userName name don't like function
